32b9d15d2b96de576e9392d79c97437e5cce20af
Taison Nomura <67935769+taison0622@users.noreply.github.com>
2021-03-19 23:46:52 +0900

    Add files via upload

---
+++ test/js_test/test.js
@@ -0,0 +1,96 @@
+//ヘッダーの位置を取得
+let navPos = jQuery('.swiper-container').offset().top;
+//ヘッダーの高さを取得
+let navHei = jQuery('.swiper-container').outerHeight();
+
+//スクロールアクション実行でスクロールTopが850pxを超えたら
+jQuery(window).on('scroll',function(){
+    if(navHei<jQuery(this).scrollTop()){
+        //jQuery( 'body' ).css( 'padding-top', navHei );
+        jQuery('#head-act').addClass('m_fixed');}
+
+    else{
+        jQuery('body').css('padding-top',0);
+        jQuery('#head-act').removeClass('m_fixed');
+    }
+});
+
+const accTrigger =
+document.querySelectorAll('.ac_trigger');
+
+accTrigger.forEach(trigger =>
+    trigger.addEventListener('click',toggleAcc));
+
+function toggleAcc(){
+    const items = document.querySelectorAll('.acc_item');
+    const thisItem = this.parentNode;
+    items.forEach(item =>{
+        if(thisItem == item){
+            thisItem.classList.toggle('is_open');
+            return;
+        }
+        item.classList.remove('is_open');
+    });
+}
+
+$(function() {
+
+	/**
+	 * 現在スクロール位置によるグローバルナビのアクティブ表示
+	 */
+	let scrollMenu = function() {
+		// 配列宣言
+		// ここにスクロールで点灯させる箇所のidを記述する
+		// 数値は全て0でOK
+		let array = {
+			'#sec1': 0,
+			'#sec2': 0,
+			'#sec3': 0,
+			'#sec4': 0,
+			'#sec5': 0,
+			'#sec6': 0,
+            '#sec7': 0
+		};
+
+		let $globalNavi = new Array();
+
+		// 各要素のスクロール値を保存
+		for (let key in array) {
+			if ($(key).offset()) {
+				array[key] = $(key).offset().top - 80; // 数値丁度だとずれるので10px余裕を作る
+				$globalNavi[key] = $('#header ul li a[href="' + key + '"]');
+			}
+		}
+
+		// スクロールイベントで判定
+		$(window).scroll(function () {
+			for (let key in array) {
+				if ($(window).scrollTop() > array[key] - 50) {
+					$('#header ul li a').each(function() {
+						$(this).removeClass('active');
+					});
+					$globalNavi[key].addClass('active');
+                    
+                    
+				}
+			}
+		});
+	}
+
+	// 実行
+	scrollMenu();
+});
+$(function() {
+	/**
+	 * ページ内スクロール
+	 */
+	$('a').click(function(){
+		let speed = 500;
+		let href= $(this).attr("href");
+		let target = $(href == "#" || href == "" ? 'html' : href);
+		let position = target.offset().top - 80;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+});
+
